00720d82852db554864b1dbeeab96b89bfb044b1
Gurkirat Singh <tbhaxor@gmail.com>
2019-12-15 23:36:44 +0530

    added 'lastError' property support

--- src/runtime.ts
+++ src/runtime.ts
@@ -1,16 +1,16 @@
 class Runtime {
   /**
    * @method getId
-   *
-   * @memberof chrome.runtime
+   * @memberof Runtime
    * @description Get the extension ID
+   * @scope chrome.runtime
    *
    * @example
    * (async (runtime) => {
    *     console.log(await runtime.getId());
    * })(new Runtime());
    */
-  public getId(callback?: (err?: Error, id?: string) => number): Promise<string> {
+  public getId(callback?: (err?: Error, id?: string) => void): Promise<string> {
     // instantiating a promise
     let promise = new Promise<string>((resolve, reject) => {
       // safeguarding
@@ -20,6 +20,7 @@ class Runtime {
         reject(new Error('Can\'t find the id of undefined "runtime"'));
       }
     });
+
     // check if callback is passed
     if (callback && typeof callback == 'function') {
       // handle promise
@@ -30,6 +31,38 @@ class Runtime {
         .catch((error) => {
           callback(error, null);
         });
+    } else {
+      // return promise otherwise
+      return promise;
+    }
+  }
+
+  /**
+   * @name getLastError
+   * @memberof Runtime
+   * @description Get the last error message
+   * @scope chrome.runtime
+   *
+   * @example
+   * (async (runtime) => {
+   *     console.log(await runtime.getLastError());
+   * })(new Runtime());
+   */
+  public getLastError(callback?: (lastError?: string) => void): Promise<string> {
+    // instantiating a promise
+    let promise = new Promise<string>((resolve) => {
+      // safeguarding
+      if (chrome.runtime.lastError && chrome.runtime.lastError.message) {
+        resolve(chrome.runtime.lastError.message);
+      } else {
+        resolve(undefined);
+      }
+    });
+
+    // check if callback is passed
+    if (callback && typeof callback == 'function') {
+      // handle promise
+      promise.then(callback);
     } else {
       // return promise otherwise
       return promise;
